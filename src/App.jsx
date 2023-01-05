47c17df4b351f4acea091f1eac45107e7d290bed
OtakuBoy420 <U28282@rts-365.dk>
2023-01-05 11:15:11 +0100

    init app with vite config, tailwind config, .env and folder structure

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+      <p>hej</p>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Heading.jsx
@@ -0,0 +1,3 @@
+function Heading({ children }) {
+  return <h1 className="font-semibold text-4xl">{children}</h1>
+}
