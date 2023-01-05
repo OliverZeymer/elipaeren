ccafafd925b26341d18f9b8e6a32539cdfe194b4
OtakuBoy420 <U28282@rts-365.dk>
2023-01-05 12:31:55 +0100

    finish tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,41 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme")
+const colors = require("tailwindcss/colors")
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: colors.black,
+        white: colors.white,
+        primary: "#141414",
+        secondary: "#808080",
+        additional: "#333333",
+      },
+      boxShadow: {
+        top: "0 -15px 20px 0px rgba(0, 0, 0, 0.1)",
+      },
+      fontFamily: {
+        sans: ["Poppins, sans-serif", ...defaultTheme.fontFamily.sans],
+      },
+      gridTemplateColumns: {
+        "auto-fit": "repeat(auto-fit, minmax(250px, 1fr));",
+        "auto-fill": "repeat(auto-fill, minmax(0, 1fr))",
+      },
+      gridTemplateRows: {
+        "auto-fit": "repeat(auto-fit, minmax(0, 1fr))",
+        "auto-fill": "repeat(auto-fill, minmax(0, 1fr))",
+      },
+      aspectRatio: {
+        "3/4": "3 / 4",
+      },
+    },
   },
-  plugins: [],
+
+  plugins: [
+    function ({ addVariant }) {
+      addVariant("child", "& > *")
+      addVariant("child-hover", "& > *:hover")
+    },
+  ],
 }
