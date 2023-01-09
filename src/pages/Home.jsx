e37dfad0beb2d5f9eddc013b1d8a58912d5fd80c
OtakuBoy420 <U28282@rts-365.dk>
2023-01-09 13:56:06 +0100

    inputfield done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,10 @@
-import { useState } from "react"
 import Heading from "../components/Heading"
-import InputField from "../components/InputField"
 export default function Home() {
-  const [inputValue, setInputValue] = useState("")
   return (
     <>
       <Heading bold h1>
         Home
       </Heading>
-      <InputField state={inputValue} setState={setInputValue} placeholder="Enter your name" />
     </>
   )
 }
