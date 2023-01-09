75f9921b9f3439130ee586c0a05b338dbc4913ae
benamager <benaprivat@gmail.com>
2023-01-09 13:29:12 +0100

    Add login page

--- src/pages/Connect.jsx
+++ src/pages/Connect.jsx
@@ -1,9 +1,12 @@
 import ComponentWrapper from "../components/ComponentWrapper";
 import Button from "../components/Button";
+import { useNavigate } from "react-router-dom";
 
 export default function Connect() {
+  const navigate = useNavigate();
+
   return (
-    <div>
+    <div className="flex flex-col">
       <ComponentWrapper
         type="div"
         className="bg-lighter p-7 rounded-3xl max-w-[340px] w-full"
@@ -13,7 +16,11 @@ export default function Connect() {
         </p>
         <p>Luckily, it is very simple!</p>
       </ComponentWrapper>
-      <Button className="bg-primary text-dark" text="Continue" />
+      <Button
+        className="bg-primary text-dark mt-2 ml-auto"
+        text="Continue"
+        onClick={() => navigate("/login")}
+      />
     </div>
   );
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,25 @@
-import Heading from "../components/Heading";
+import ComponentWrapper from "../components/ComponentWrapper";
+import Button from "../components/Button";
 
 export default function Login() {
-  return <Heading>Log In</Heading>;
+  return (
+    <div className="flex flex-col">
+      <ComponentWrapper
+        type="div"
+        className="bg-lighter p-7 rounded-3xl max-w-[340px] w-full"
+      >
+        <p className="mb-5">
+          Make sure your bridge is{" "}
+          <span className="font-medium">powered on</span> and{" "}
+          <span className="font-medium">ethernet plugged in.</span>
+        </p>
+        <p className="mb-5">
+          Now <span className="text-primary">press the bridge button</span> and{" "}
+          <span className="text-primary">make sure blue light blinks.</span>
+        </p>
+        <p>Well done, continue.</p>
+      </ComponentWrapper>
+      <Button className="bg-primary text-dark mt-2 ml-auto" text="Continue" />
+    </div>
+  );
 }
