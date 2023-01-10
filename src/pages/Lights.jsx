2829abb44b904b00574f6129be1c18da8219ed07
benamager <benaprivat@gmail.com>
2023-01-10 14:07:07 +0100

    Add light component

--- src/pages/Lights.jsx
+++ src/pages/Lights.jsx
@@ -2,8 +2,20 @@ import Heading from "../components/Heading";
 import ComponentWrapper from "../components/ComponentWrapper";
 import ScrollContainer from "../components/ScrollContainer";
 import Tag from "../components/Tag";
+import Light from "../components/Light";
+import { useContext } from "react";
+import useAxios from "../hooks/useAxios";
+import IpContext from "../contexts/IpContext";
+import TokenContext from "../contexts/TokenContext";
 
 export default function Lights() {
+  const { bridgeIpContext } = useContext(IpContext);
+  const { token } = useContext(TokenContext);
+
+  const { data, loading, error, put } = useAxios(
+    `${bridgeIpContext}/api/${token}/lights`
+  );
+
   return (
     <section className="w-full max-w-full">
       <Heading>Lights (6)</Heading>
@@ -12,7 +24,10 @@ export default function Lights() {
         <Tag text="All lights off" className="bg-primary" />
         <Tag text="Add to room" className="bg-grey" />
         <Tag text="Add to new room" className="bg-grey" />
+        <Tag text="Add to new room" className="bg-grey" />
       </ScrollContainer>
+      <Light text="#1" isOn={true} selected={false} />
+      <Light text="#2" isOn={false} selected={true} />
     </section>
   );
 }
