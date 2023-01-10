8799055cabffbb36f9ef5099085d42b3a8b96db6
benamager <benaprivat@gmail.com>
2023-01-10 11:11:25 +0100

    Add logout and ip context

--- src/ConnectLayout.jsx
+++ src/ConnectLayout.jsx
@@ -3,7 +3,6 @@ import Logo from "./components/Logo";
 
 export default function ConnectLayout(props) {
   const { children } = props;
-  console.log(children);
   return (
     <div className="max-w-5xl h-full min-h-screen mx-auto p-4 flex">
       <main className="w-full max-w-[340px] mx-auto my-auto flex flex-col items-center">
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,19 +1,39 @@
-import Button from "./Button"
-import ComponentWrapper from "./ComponentWrapper"
-import Logo from "./Logo"
-import NavigationLink from "./NavigationLink"
+import Button from "./Button";
+import ComponentWrapper from "./ComponentWrapper";
+import Logo from "./Logo";
+import NavigationLink from "./NavigationLink";
+import TokenContext from "../contexts/TokenContext";
+import { useContext } from "react";
+import { setCookie } from "react-use-cookie";
 
 const navigationLinks = [
   { path: "/", name: "Home" },
   { path: "/lights", name: "Lights" },
   { path: "/rooms", name: "Rooms" },
-]
+];
 
 export default function Navigation() {
+  const { setToken } = useContext(TokenContext);
+
+  function logMeOut() {
+    setCookie("hueToken", "", {
+      days: 365,
+      SameSite: "Lax",
+      Secure: true,
+    });
+    setCookie("bridgeIp", "", {
+      days: 365,
+      SameSite: "Lax",
+      Secure: true,
+    });
+    setToken(null);
+  }
+
   return (
     <ComponentWrapper
       type="nav"
-      className="z-50 flex-row rounded-3xl fixed bottom-0 flex select-none sm:static sm:flex w-screen bg-lighter sm:p-6 sm:max-w-xs sm:flex-col sm:h-[97vh]">
+      className="z-50 flex-row rounded-3xl fixed bottom-0 flex select-none sm:static sm:flex w-screen bg-lighter sm:p-6 sm:max-w-xs sm:flex-col sm:h-[97vh]"
+    >
       <Logo className="mx-auto mb-12" />
       <ul className="flex sm:gap-6 sm:flex-col sm:h-full">
         {navigationLinks.map((link) => (
@@ -22,7 +42,11 @@ export default function Navigation() {
           </li>
         ))}
       </ul>
-      <Button text="Show Tutorial" className="hidden sm:block bg-black text-xl !p-4 text-left" />
+      <Button
+        text="Log out"
+        className="hidden sm:block bg-black text-xl !p-4 text-left"
+        onClick={() => logMeOut()}
+      />
     </ComponentWrapper>
-  )
+  );
 }
